14d69211d328c9e8f36cf767565fd1bb490cefa3
gui <guipereira05122005@gmail.com>
2024-02-01 14:02:47 -0300

    teste

---
+++ test/server.js
@@ -0,0 +1,10 @@
+import * as chai from "chai"
+const expect = chai.expect
+
+describe('Soma', () => {
+    it("Somas", function(){
+        const resultado = 0 + 5
+        expect(resultado).be.equal(5)
+        done()
+    })
+})
